edfb4eb796cdfeb112516ea67b87706535b07571
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-14 21:02:35 +0200

    Refactor uploadFiles function to include file format validation and dynamic user directory creation

--- src/Utils/multer.utils.js
+++ src/Utils/multer.utils.js
@@ -1,9 +1,27 @@
 import multer, { diskStorage } from "multer";
-
-export const uploadFiles = () => {
+import fs from "node:fs";
+import { BadRequestException } from "./Response/error.response";
+export const uploadFiles = (
+  allowedFormat = ["image/png", "image/jpg", "image/jpeg", "image/gif"],
+) => {
   return multer({
+    fileFilter: (req, file, cb) => {
+      if (!allowedFormat.includes(file.mimetype)) {
+        return cb(
+          BadRequestException({ message: "Invalid file format!" }),
+          false,
+        );
+      }
+
+      cb(null, true);
+    },
     storage: diskStorage({
-      destination: "uploads",
+      destination: (req, file, cb) => {
+        if (!fs.existsSync(`upload/${req.user.id}`)) {
+          fs.mkdirSync(`upload/${req.user.id}`);
+        }
+        cb(null, `upload/${req.user.id}`);
+      },
       filename: (req, file, cb) => {
         cb(null, Date.now() + Math.random() + "__" + file.originalname);
       },
