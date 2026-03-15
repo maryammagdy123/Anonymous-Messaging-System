cb4df110ca5d656b24be989423f834eeef9882ff
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-15 03:18:29 +0200

    Add file validation middleware to check file types during uploads

---
+++ src/Middleware/file-validation.middleware.js
@@ -0,0 +1,19 @@
+import { fileTypeFromBuffer } from "file-type"; //npm i file-type
+import fs from "node:fs";
+import { BadRequestException } from "../Utils/Response/error.response.js";
+
+// Middleware to validate file type by magic number (file signatures)
+export const fileValidation = async (req, res, next) => {
+  // get the file path
+  const filePath = req.file.path;
+  // read the file and return buffer
+  const buffer = fs.readFileSync(filePath);
+  // get the file type
+  const type = await fileTypeFromBuffer(buffer);
+  // validate
+  const allowedTypes = ["image/jpeg", "image/png"];
+  if (!type || !allowedTypes.includes(type.mime))
+    return BadRequestException({ message: "Invalid file type!" });
+
+  return next();
+};
