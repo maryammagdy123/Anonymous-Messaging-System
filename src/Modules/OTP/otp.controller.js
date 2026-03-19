ccf8d1381b1a1d4fae39f50b2410a6117c40cb46
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-19 22:16:28 +0200

    refactor verifyEmail controller response

--- src/Modules/OTP/otp.controller.js
+++ src/Modules/OTP/otp.controller.js
@@ -2,12 +2,12 @@ import { successResponse } from "../../Utils/Response/success.response.js";
 import * as service from "./otp.services.js";
 export const verifyEmail = async (req, res, next) => {
   try {
-    const { email, otp } = req.body;
-    await service.verifyOTP(otp, email, "verify");
+    const verified=await service.verifyOTP(req.body, "verify");
     return successResponse({
       res,
       status: 200,
       message: "Your Account verified successfully !",
+      data:{verified:verified.acknowledged}
     });
   } catch (error) {
     next(error);
