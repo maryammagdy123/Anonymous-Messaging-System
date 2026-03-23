99333624daa19f8b3ee6d1d38744c4a34a26e055
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-03-23 18:44:03 +0200

    `Added delete statement for 'isConfirmed' property in getUserWithNoSensitiveData function`

--- src/Modules/User/user.services.js
+++ src/Modules/User/user.services.js
@@ -26,6 +26,7 @@ export const getUserWithNoSensitiveData = (user) => {
   delete userObj.publicKey;
   delete userObj.profileVisits;
   delete userObj.provider;
+  delete userObj.isConfirmed;
 
   return userObj;
 };
